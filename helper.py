c08fe0b7136e69a212edcee260d930682a815835
karamathasan <karamathasan420@gmail.com>
2024-07-16 12:36:49 -0400

    data shuffling and testing

--- helper.py
+++ helper.py
@@ -1,4 +1,28 @@
 import pandas as pd
+import numpy as np
 
-def shuffle(data):
-    pass
+def dataSplit(predictor_data: list, effector_data: pd.DataFrame, percentageTraining = 0.6):
+    assert(len(predictor_data) == len(effector_data))
+    rows = len(predictor_data)
+    # shuffle(predictor_data, effector_data, shuffleProportion)
+    training_predictor = predictor_data[0:int(rows * percentageTraining)]
+    training_effector = effector_data.iloc[0:int(rows * percentageTraining)]
+    testing_predictor = predictor_data[int(rows * percentageTraining): rows]
+    testing_effector = effector_data.iloc[int(rows * percentageTraining): rows]
+    return training_predictor, training_effector, testing_predictor, testing_effector
+
+def shuffle(predictor_data:list, effector_data: pd.DataFrame, shuffleProportion = 0.5):
+    assert len(predictor_data) == len(effector_data)
+    rows = len(predictor_data)
+    shuffleiterations = int(rows * shuffleProportion)
+    for i in range(shuffleiterations):
+        u = np.random.randint(0,len(predictor_data))
+        v = np.random.randint(0,len(predictor_data))
+
+        temp = predictor_data[u]
+        predictor_data[u] = predictor_data[v]
+        predictor_data[v] = temp
+        
+        temp = effector_data.iloc[u]
+        effector_data.iloc[u] = effector_data.iloc[v]
+        effector_data.iloc[v] = temp
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import PIL.Image
 import tensorflow as tf
 import keras
 from keras import Sequential 
 from keras import layers
 from keras import models
 
-import PIL
-import pathlib
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-import matplotlib.pyplot as plt
+import PIL
+import PIL.Image
+import pathlib
+
+from helper import shuffle, dataSplit
+
 print("\n\n\n\n")
 
 # we have a csv of image links and the species of butterfly in the image
@@ -27,8 +30,8 @@ model = Sequential([
   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-#   layers.Conv2D(32, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
 #   layers.Conv2D(64, 3, padding='same', activation='relu'),
 #   layers.MaxPooling2D(),
   layers.Flatten(),
@@ -41,44 +44,50 @@ model.compile(\
     metrics=[keras.metrics.CategoricalAccuracy()]\
     )
 
-imgs = pathlib.Path("data/butterflyData")
-train_imgs = list(imgs.glob('train/*'))
-test_imgs = list(imgs.glob('test/*'))
+dir = pathlib.Path("data/butterflyData")
+imgs = list(dir.glob('train/*'))
+# test_imgs = list(dir.glob('test/*'))
 
-# epochs = 10
-# files = data['filename'][0:100]
+classification = pd.get_dummies(data, columns=["label"], dtype=int)
+classification = classification.drop("filename", axis=1)
 
-# labelDataSet = data['label'][0:100]
-one_hot_train = pd.get_dummies(data, columns=["label"], dtype=int)
-one_hot_train = one_hot_train.drop("filename", axis=1)
-one_hot_train = one_hot_train.iloc[0:100]
+training_imgs, training_classes, testing_imgs, testing_classes = dataSplit(imgs, classification)
 
-# print(one_hot.head())
+# /////// ITERATIVE FITTING ///////
+for j in range(1):
+    shuffle(training_imgs, training_classes)
+    # for i in range(len(training_imgs)):
+    for i in range(100):
+        img = PIL.Image.open(training_imgs[i])
+        img = keras.utils.img_to_array(img)
+        y = training_classes.iloc[i].transpose()
 
-for i in range(100):
-    img = PIL.Image.open(train_imgs[i])
-    img = keras.utils.img_to_array(img)
-    y = one_hot_train.iloc[i].transpose()
-    # print(f"img shape: {img.shape}")
-    # print(f"classification shape: {y.shape}")
-    # plt.imshow(keras.utils.img_to_array(img, dtype=int))
-    # plt.title(data["label"].iloc[i])
-    # plt.show()
+        img = np.expand_dims(img, axis = 0)
+        y = np.expand_dims(y, axis = 0)
+        model.fit(img,y,batch_size=1)
 
-    img = np.expand_dims(img, axis = 0)
-    y = np.expand_dims(y, axis = 0)
-    model.fit(img,y,batch_size=1)
+score = 0
+iterations = 100
+for i in range(iterations):
+    img = PIL.Image.open(testing_imgs[i])
+    prediction = model.predict(np.expand_dims(keras.utils.img_to_array(img,dtype=float ),axis = 0))
+    true_class = classification.columns[np.argmax(testing_classes.iloc[i])]
+    predicted_class = classification.columns[np.argmax(prediction)]
+    # print(f'    prediction: {prediction}')
+    print()
+    print(f'    true class: {true_class}, predicted class: {predicted_class}')
+    if (testing_classes.iloc[i].to_numpy().all() == prediction.all()):
+        score+=1
+print(f"accuracy: {100 * (score / iterations)}%")
 
-testData = data = pd.read_csv("data/butterflyData/Testing_set.csv")
+# for i in range(10):
+#     img = PIL.Image.open(testing_imgs[i])
+#     prediction = model.predict(np.expand_dims(keras.utils.img_to_array(img, dtype=float),axis=0))
+#     max_index = np.argmax(prediction)
 
-for i in range(10):
-    img = PIL.Image.open(test_imgs[i])
-    prediction = model.predict(np.expand_dims(keras.utils.img_to_array(img, dtype=float),axis=0))
-    max_index = np.argmax(prediction)
+#     prediction = classification.columns[max_index]
+#     img = keras.utils.img_to_array(img, dtype=int)
 
-    prediction = one_hot_train.columns[max_index]
-    img = keras.utils.img_to_array(img, dtype=int)
-
-    plt.imshow(img)
-    plt.title(prediction)
-    plt.show()
+#     plt.imshow(img)
+#     plt.title(prediction)
+#     plt.show()
