b519b5b97fad93d3be0e97be3a67380a0af6d31c
karamathasan <karamathasan420@gmail.com>
2024-07-17 15:52:36 -0400

    new model for flower data

--- main.py
+++ main.py
@@ -12,19 +12,16 @@ import PIL
 import PIL.Image
 import pathlib
 
-from helper import shuffle, dataSplit
-
 print("\n\n\n\n")
 
-# we have a csv of image links and the species of butterfly in the image
-# we can make a conv net to train on each image as we iterate through the csv, calulate error with the labels
-# after using the labels, test on the other csv and then record the accuracy
-# bonus is to use unsupervised learning
-data = pd.read_csv("data/butterflyData/Training_set.csv")
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos.tar', origin=dataset_url, extract=True)
+data_dir = pathlib.Path(data_dir).with_suffix('')
 
-img_height = 224
-img_width = 224
-num_classes = 75
+batch_size = 32
+img_height = 180
+img_width = 180
+num_classes = 5
 
 model = Sequential([
   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
@@ -32,62 +29,70 @@ model = Sequential([
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-#   layers.Conv2D(64, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
 model.compile(\
     optimizer='adam',\
-    loss = keras.losses.CategoricalCrossentropy(from_logits=True),\
-    metrics=[keras.metrics.CategoricalAccuracy()]\
+    loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True),\
+    metrics=['accuracy']\
     )
 
-dir = pathlib.Path("data/butterflyData")
-imgs = list(dir.glob('train/*'))
-# test_imgs = list(dir.glob('test/*'))
 
-classification = pd.get_dummies(data, columns=["label"], dtype=int)
-classification = classification.drop("filename", axis=1)
+train_ds = keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-training_imgs, training_classes, testing_imgs, testing_classes = dataSplit(imgs, classification)
+val_ds = keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-# /////// ITERATIVE FITTING ///////
-for j in range(1):
-    shuffle(training_imgs, training_classes)
-    # for i in range(len(training_imgs)):
-    for i in range(100):
-        img = PIL.Image.open(training_imgs[i])
-        img = keras.utils.img_to_array(img)
-        y = training_classes.iloc[i].transpose()
+AUTOTUNE = tf.data.AUTOTUNE
 
-        img = np.expand_dims(img, axis = 0)
-        y = np.expand_dims(y, axis = 0)
-        model.fit(img,y,batch_size=1)
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-score = 0
-iterations = 100
-for i in range(iterations):
-    img = PIL.Image.open(testing_imgs[i])
-    prediction = model.predict(np.expand_dims(keras.utils.img_to_array(img,dtype=float ),axis = 0))
-    true_class = classification.columns[np.argmax(testing_classes.iloc[i])]
-    predicted_class = classification.columns[np.argmax(prediction)]
-    # print(f'    prediction: {prediction}')
-    print()
-    print(f'    true class: {true_class}, predicted class: {predicted_class}')
-    if (testing_classes.iloc[i].to_numpy().all() == prediction.all()):
-        score+=1
-print(f"accuracy: {100 * (score / iterations)}%")
+epochs=10
+# print(type(train_ds))
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+model.summary()
 
-# for i in range(10):
-#     img = PIL.Image.open(testing_imgs[i])
-#     prediction = model.predict(np.expand_dims(keras.utils.img_to_array(img, dtype=float),axis=0))
-#     max_index = np.argmax(prediction)
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
-#     prediction = classification.columns[max_index]
-#     img = keras.utils.img_to_array(img, dtype=int)
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-#     plt.imshow(img)
-#     plt.title(prediction)
-#     plt.show()
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # Purpose
-the purpose of this project was to build stronger comfortability with Tensorflow and CNNs. The goal is to classify butterflies by species given labeled images
+the purpose of this project was to build stronger comfortability with Tensorflow and CNNs. The goal is to classify flowers 
 # Resources
 the data was found [here](https://www.kaggle.com/datasets/phucthaiv02/butterfly-image-classification?select=Training_set.csv)
