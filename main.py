2707ec40d4891c544955179c89da9ec3d351ce12
karamathasan <karamathasan420@gmail.com>
2024-07-20 14:37:27 -0400

    added data augmentation: accuracy at ~75%

--- main.py
+++ main.py
@@ -25,13 +25,19 @@ num_classes = 5
 
 model = Sequential([
   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  # augment data
+  layers.RandomFlip('horizontal',input_shape=(img_height, img_width, 3)),
+  layers.RandomZoom(0.5),
+  layers.RandomRotation(0.5),
+
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.Dropout(0.2),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.Dropout(0.4),
   layers.MaxPooling2D(),
-  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -64,7 +70,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-epochs=10
+epochs=30
 # print(type(train_ds))
 history = model.fit(
   train_ds,
@@ -94,5 +100,3 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-
-
